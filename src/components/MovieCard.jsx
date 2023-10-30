48fd5994b11631c5d5e324da0407e6f125418759
CamiFlo <camila.flores.j@gmail.com>
2023-10-30 19:55:44 -0300

    endpoint discover/movie trae la data y se renderiza en home

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import styles from "./MovieCard.module.css";
 import { Link } from "react-router-dom";
 
-export default function MovieCard({ title, release_date, poster_path }) {
+export default function MovieCard({ movie }) {
   //const imageUrl = "http://image.tmdb.org/t/p/w500" +
   return (
     <li className={styles.movieCard}>
-      <Link to={'/movie/'+ movie.id}> 
+      
       <img
         className={styles.moviePoster}
-        src={`https://www.themoviedb.org/t/p/original/${poster_path}`}
-        alt={`poster ${title}`}
+        src={`https://www.themoviedb.org/t/p/w500/${movie.poster_path}`}
+        alt={`poster ${movie.title}`}
         width={230}
         height={345}
       />
-      </Link>
-      <h4 className={styles.movieTitle}>{title}</h4>
-      <h5 className={styles.movieYear}>{release_date}</h5>
+      
+      <h4 className={styles.movieTitle}>{movie.title}</h4>
+      <h5 className={styles.movieYear}>{movie.release_date}</h5>
     </li>
   );
 }
+
+//<Link to={`/movie/${movie.id}`}> 
+//</Link>
