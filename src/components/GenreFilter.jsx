f71a097a83b927b5fa94c34756a4fd41c7d671b0
CamiFlo <camila.flores.j@gmail.com>
2024-01-03 17:22:39 -0300

    genre filter (me faltó hacer este commit en noviembre)

--- src/components/GenreFilter.jsx
+++ src/components/GenreFilter.jsx
@@ -1,17 +1,29 @@
-import styles from './GenreFilter.module.css'
+import styles from './GenreFilter.module.css';
 
-export default function GenreFilter({ genres, selectedGenre, onSelectGenre }) {
+import { useState } from "react";
+
+export default function GenreFilter({ genres, onSelectGenre }) {
+  const [selectedGenre, setSelectedGenre] = useState("all");
+
+  const handleGenreChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedGenre(selectedValue);
+    onSelectGenre(selectedValue);
+  };
+
   return (
-    <div>
+    <div className={styles.genreFilterContainer}>
       <span className={styles.filterTitle}>Filter by Genre </span>
-      <select value={selectedGenre} onChange={onSelectGenre}>
-        <option value="all">All</option>
+      <select value={selectedGenre} onChange={handleGenreChange}>
+        <option className={styles.option} value="all">
+          All
+        </option>
         {genres.map((genre) => (
-          <option key={genre.id} value={genre.id}>
+          <option className={styles.option} key={genre.id} value={genre.id}>
             {genre.name}
           </option>
         ))}
       </select>
     </div>
   );
-};
+}
